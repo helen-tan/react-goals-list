f9e1984dbd12fd54d8dccf3754ce17bef95c8f3a
helen tan <helentanmz0909@gmail.com>
2022-09-07 18:05:44 +0800

    Created goal list component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import GoalList from './components/GoalList';
 
 function App() {
   return (
-    <div>App</div>
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
   );
 }
 
---
+++ src/components/GoalList.jsx
@@ -0,0 +1,13 @@
+import './GoalList.css'
+
+const GoalList = () => {
+  return (
+    <ul className='goal-list'>
+      <li>Finish the Course</li>
+      <li>Learn about how each part of the MERN stack connect with eah other</li>
+      <li>How to build a REST API with NodeJS and Express</li>
+    </ul>
+  )
+}
+
+export default GoalList

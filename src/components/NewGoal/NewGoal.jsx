624f74f7a43131b3ce22dc931657c351b5e2242a
helen tan <helentanmz0909@gmail.com>
2022-09-08 14:32:20 +0800

    Fetch user input (two-way binding)

--- src/components/NewGoal/NewGoal.jsx
+++ src/components/NewGoal/NewGoal.jsx
@@ -1,23 +1,35 @@
+import { useState } from 'react'
 import './NewGoal.css'
 
 const NewGoal = ({ onAddGoal }) => {
+  const [enteredText, setEnteredText] = useState('')
+
+  const textChangeHandler = (e) => {
+    setEnteredText(e.target.value)
+  }
+
   const addGoalHandler = (e) => {
     e.preventDefault() // prevent browser default of sending a req to the backend
 
     const newGoal = {
-      id: Math.random()*10,
-      text: 'A new goal'
+      id: Math.random() * 10,
+      text: enteredText
     }
+
+    setEnteredText("")
 
     onAddGoal(newGoal)
   }
 
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler}/>
       <button type='submit'>Add Goal</button>
     </form>
   )
 }
 
 export default NewGoal
+
+// value property: tell input component that value displayed in the input should be the value stored in enteredText

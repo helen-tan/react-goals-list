c3ad492bb2c11541c4bde4d6e8bc92d7e887162a
helen tan <helentanmz0909@gmail.com>
2022-09-08 13:53:09 +0800

    pass data from child to parent compontn via event handler prop

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ function App() {
     {id: 3, text: 'How to build a REST API with NodeJS and Express'},
   ];
 
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal)
+    console.log(courseGoals)
+  }
+
   return (
     <div className='course-goals'>
       <h2>Course Goals</h2>
-      <NewGoal />
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals} />
     </div>
   );
--- src/components/NewGoal/NewGoal.jsx
+++ src/components/NewGoal/NewGoal.jsx
@@ -1,8 +1,19 @@
 import './NewGoal.css'
 
-const NewGoal = () => {
+const NewGoal = ({ onAddGoal }) => {
+  const addGoalHandler = (e) => {
+    e.preventDefault() // prevent browser default of sending a req to the backend
+
+    const newGoal = {
+      id: Math.random()*10,
+      text: 'A new goal'
+    }
+
+    onAddGoal(newGoal)
+  }
+
   return (
-    <form className="new-goal">
+    <form className="new-goal" onSubmit={addGoalHandler}>
       <input type="text" />
       <button type='submit'>Add Goal</button>
     </form>
